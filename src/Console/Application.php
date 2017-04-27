86a36b26ddd8db1061cc72c7abc1746682ddfa4e
Povilas Susinskas <povilas.susinskas@gear4music.com>
2017-04-27 11:34:00 +0100

    Upgrade Version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Application extends BaseApplication
 {
-    const VERSION = '1.0.2';
+    const VERSION = '1.0.3';
     const COMMAND_NAME = 'phpmnd';
 
     public function __construct()
