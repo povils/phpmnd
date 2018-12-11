aea0958cdad57b26081f17beb190d4f3def7320b
Scott Dutton <scott@exussum.co.uk>
2018-12-11 21:59:47 +0000

    Check const for arrays
    Example
    
    const something = 2; should be accepted but having a const array should
    not
    
    const something = [1,2,3]; should be something like
    
    const number = 2;
    const something = [self::number];

--- tests/DetectorTest.php
+++ tests/DetectorTest.php
@@ -302,6 +302,25 @@ class DetectorTest extends TestCase
         );
     }
 
+    public function testCheckForMagicArrayConstants(): void
+    {
+        $option = $this->createOption();
+        $option->setExtensions([new ArrayExtension()]);
+        $detector = $this->createDetector($option);
+
+        $fileReport = $detector->detect(FileReportTest::getTestFile('test_3'));
+        var_dump($fileReport->getEntries());
+
+        $this->assertContains(
+            [
+                'line' => 4,
+                'value' => 2,
+            ],
+            $fileReport->getEntries()
+        );
+
+    }
+
     private function createOption(array $extensions = []): Option
     {
         $option = new Option;
