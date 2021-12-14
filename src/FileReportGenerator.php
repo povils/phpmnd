b67adb13a67d1aa49c58003825b1ccdf2d51e3ec
Scott Dutton <scott@exussum.co.uk>
2021-12-14 13:59:52 +0000

    update PR

--- src/FileReportGenerator.php
+++ src/FileReportGenerator.php
@@ -27,6 +27,12 @@ class FileReportGenerator
      */
     private $option;
 
+    private $baseLookup = [
+        2 => 'b',
+        8 => 'o',
+        16 => 'x',
+    ];
+
     public function __construct(SplFileInfo $file, Option $option)
     {
         $this->file = $file;
@@ -59,6 +65,21 @@ class FileReportGenerator
                 $extension->setOption($this->option);
 
                 if ($extension->extend($node)) {
+                    if ((
+                        $scalar instanceof LNumber &&
+                        $scalar->getAttribute('kind') !== LNumber::KIND_DEC
+                    )) {
+                        $scalar->value = sprintf(
+                            '0%s%d',
+                            $this->baseLookup[$scalar->getAttribute('kind')],
+                            base_convert(
+                                $scalar->value,
+                                LNumber::KIND_DEC,
+                                $scalar->getAttribute('kind')
+                            )
+                        );
+                    }
+
                     yield new DetectionResult($this->file, $scalar->getLine(), $scalar->value);
                 }
             }
