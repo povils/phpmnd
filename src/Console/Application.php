6b12352d809dbcbea6e9c2ff34d8cf38cfd16bce
Laurent Laville <laurent.laville@gmail.com>
2024-01-09 08:57:04 +0000

    add test to verify that Application::VERSION match the latest git tag

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends BaseApplication
 {
-    public const VERSION = '3.3.0';
+    public const VERSION = '3.4.0';
     private const NAME = 'phpmnd';
 
     private Container $container;
