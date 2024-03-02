1f3eb869e3a2c4fddbe0995fddd97b75caa2c203
Oleg Zhulnev <plbSiD@gmail.com>
2024-03-02 20:53:44 +0300

    Bump version to 3.4.1 (#177)

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends BaseApplication
 {
-    public const VERSION = '3.4.0';
+    public const VERSION = '3.4.1';
     private const NAME = 'phpmnd';
 
     private Container $container;
